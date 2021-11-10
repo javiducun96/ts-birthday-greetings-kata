834d7ae5a81d98e5869d563a3b9274767bbaa368
iraticasi <irati.casi@540deg.com>
2021-11-10 13:33:26 +0100

    extraer obtencion de empleados a infraestuctura

---
+++ src/infrastucture/FileEmployeeRepository.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import {Employee} from 'domain/Employee';
+import {OurDate} from 'domain/OurDate';
+
+export class FileEmployeeRepository {
+    getByBirthday(ourDate: OurDate) {
+        // get employees from file
+        const data = fs.readFileSync(path.resolve(__dirname, './_resources/employee_data.txt'), 'UTF-8')
+        const lines = data.split(/\r?\n/)
+        lines.shift()
+        const employees = lines.map(line => {
+            const employeeData = line.split(', ')
+            return new Employee(employeeData[1], employeeData[0], employeeData[2], employeeData[3])
+        })
+
+        // filter employees
+        return employees.filter(employee => employee.isBirthday(ourDate))
+    }
+
+}
--- src/services/BirthdayService.ts
+++ src/services/BirthdayService.ts
@@ -1,14 +1,13 @@
-import fs from 'fs'
-import path from 'path'
 import nodemailer from 'nodemailer'
 import Mail from 'nodemailer/lib/mailer'
 import SMTPTransport from 'nodemailer/lib/smtp-transport'
-import { Employee } from 'domain/Employee'
-import { OurDate } from 'domain/OurDate'
+import {OurDate} from 'domain/OurDate'
+import {FileEmployeeRepository} from 'infrastucture/FileEmployeeRepository';
 
 export class BirthdayService {
-    sendGreetings( ourDate: OurDate, smtpHost: string, smtpPort: number) {
-        const birthdayEmployees = getEmployeesByBirthday(ourDate)
+    sendGreetings(ourDate: OurDate, smtpHost: string, smtpPort: number) {
+        const fileEmployeeRepository = new FileEmployeeRepository()
+        const birthdayEmployees = fileEmployeeRepository.getByBirthday(ourDate)
 
         // send emails
         birthdayEmployees.forEach((employee) => {
@@ -21,7 +20,7 @@ export class BirthdayService {
     }
 
     async sendMessage(smtpHost: string, smtpPort: number, sender: string,
-        subject: string, body: string, recipient: string) {
+                      subject: string, body: string, recipient: string) {
 
         const message = {
             host: smtpHost,
@@ -46,17 +45,3 @@ export class BirthdayService {
 export interface Message extends SMTPTransport.Options, Mail.Options {
 }
 
-
-const getEmployeesByBirthday = (ourDate: OurDate) => {
-    // get employees from file
-    const data = fs.readFileSync(path.resolve(__dirname, '../../resources/employee_data.txt'), 'UTF-8')
-    const lines = data.split(/\r?\n/)
-    lines.shift()
-    const employees = lines.map(line => {
-        const employeeData = line.split(', ')
-        return new Employee(employeeData[1], employeeData[0], employeeData[2], employeeData[3])
-    })
-
-    // filter employees
-    return employees.filter(employee => employee.isBirthday(ourDate))
-}
