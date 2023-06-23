cdd9394dc00083a17cfaf2ac04497825576767d6
javiducun <javiducun@gmail.com>
2023-06-23 17:50:00 +0200

    use candy syntax on Employee consturctor

--- src/Employee.ts
+++ src/Employee.ts
@@ -1,49 +1,47 @@
-import { OurDate } from './OurDate'
+import { OurDate } from "./OurDate"
 
 export class Employee {
+  private readonly _birthDate: OurDate
 
-    private readonly _firstName: string
-    private readonly _lastName: string
-    private readonly _birthDate: OurDate
+  constructor(
+    private readonly _firstName: string,
+    private readonly _lastName: string,
+    birthDate: string,
     private readonly _email: string
+  ) {
+    this._birthDate = new OurDate(birthDate)
+  }
 
-    constructor(firstName: string, lastName: string, birthDate: string, email: string) {
-        this._firstName = firstName
-        this._lastName = lastName
-        this._birthDate = new OurDate(birthDate)
-        this._email = email
+  getFirstName(): string {
+    return this._firstName
+  }
+
+  getLastName(): string {
+    return this._lastName
+  }
+
+  getEmail(): string {
+    return this._email
+  }
+
+  isBirthday(today: OurDate) {
+    return today.isSameDay(this._birthDate)
+  }
+
+  equals(obj: object) {
+    if (!(obj instanceof Employee)) {
+      return false
     }
 
-    getFirstName(): string {
-        return this._firstName
+    if (!this._birthDate.equals(obj._birthDate)) {
+      return false
     }
-
-    getLastName(): string {
-        return this._lastName
+    if (this._email !== obj._email) {
+      return false
     }
-
-    getEmail(): string {
-        return this._email
+    if (this._firstName !== obj._firstName) {
+      return false
     }
-
-    isBirthday(today: OurDate) {
-        return today.isSameDay(this._birthDate)
-    }
-
-    equals(obj: object) {
-        if (!(obj instanceof Employee)) {
-            return false
-        }
-
-        if (!this._birthDate.equals(obj._birthDate)) {
-            return false
-        }
-        if (this._email !== obj._email) {
-            return false
-        }
-        if (this._firstName !== obj._firstName) {
-            return false
-        }
-        return this._lastName === obj._lastName
-    }
+    return this._lastName === obj._lastName
+  }
 }
