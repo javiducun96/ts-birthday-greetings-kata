bb651efd7f78d924d35e3016ab54db20234b8366
javiducun <javiducun@gmail.com>
2023-06-23 17:51:06 +0200

    naming

--- src/BirthdayService.ts
+++ src/BirthdayService.ts
@@ -1,54 +1,80 @@
-import fs from 'fs'
-import path from 'path'
-import nodemailer from 'nodemailer'
-import {Employee} from './Employee'
-import {OurDate} from './OurDate'
-import Mail from 'nodemailer/lib/mailer'
-import SMTPTransport from 'nodemailer/lib/smtp-transport'
+import fs from "fs"
+import path from "path"
+import nodemailer from "nodemailer"
+import { Employee } from "./Employee"
+import { OurDate } from "./OurDate"
+import Mail from "nodemailer/lib/mailer"
+import SMTPTransport from "nodemailer/lib/smtp-transport"
 
 export class BirthdayService {
-    sendGreetings(fileName: string, ourDate: OurDate, smtpHost: string, smtpPort: number) {
-        const data = fs.readFileSync(path.resolve(__dirname, `../resources/${fileName}`), 'UTF-8')
-        // split the contents by new line
-        const lines = data.split(/\r?\n/)
-        lines.shift()
+  sendGreetings(
+    fileName: string,
+    ourDate: OurDate,
+    smtpHost: string,
+    smtpPort: number
+  ) {
+    const data = fs.readFileSync(
+      path.resolve(__dirname, `../resources/${fileName}`),
+      "UTF-8"
+    )
+    // split the contents by new line
+    const employeesRows = data.split(/\r?\n/)
+    employeesRows.shift()
 
-        // print all lines
-        lines.forEach((line) => {
-            const employeeData = line.split(', ')
-            const employee = new Employee(employeeData[1], employeeData[0], employeeData[2], employeeData[3])
-            if (employee.isBirthday(ourDate)) {
-                const recipient = employee.getEmail()
-                const body = 'Happy Birthday, dear %NAME%!'.replace('%NAME%',
-                    employee.getFirstName())
-                const subject = 'Happy Birthday!'
-                this.sendMessage(smtpHost, smtpPort, 'sender@here.com', subject, body, recipient)
-            }
-        })
+    // print all lines
+    employeesRows.forEach((employeeRow) => {
+      const employeeData = employeeRow.split(", ")
+      const employee = new Employee(
+        employeeData[1],
+        employeeData[0],
+        employeeData[2],
+        employeeData[3]
+      )
+      if (employee.isBirthday(ourDate)) {
+        const recipient = employee.getEmail()
+        const body = "Happy Birthday, dear %NAME%!".replace(
+          "%NAME%",
+          employee.getFirstName()
+        )
+        const subject = "Happy Birthday!"
+        this.sendMessage(
+          smtpHost,
+          smtpPort,
+          "sender@here.com",
+          subject,
+          body,
+          recipient
+        )
+      }
+    })
+  }
+
+  async sendMessage(
+    smtpHost: string,
+    smtpPort: number,
+    sender: string,
+    subject: string,
+    body: string,
+    recipient: string
+  ) {
+    const message = {
+      host: smtpHost,
+      port: smtpPort,
+      from: sender,
+      to: [recipient],
+      subject,
+      text: body,
     }
 
-    async sendMessage(smtpHost: string, smtpPort: number, sender: string,
-                      subject: string, body: string, recipient: string) {
+    this.deliveryMessage(message)
+  }
 
-        const message = {
-            host: smtpHost,
-            port: smtpPort,
-            from: sender,
-            to: [recipient],
-            subject,
-            text: body
-        }
+  // made protected for testing :-(
+  protected async deliveryMessage({ host, port, ...msg }: Message) {
+    const transport = nodemailer.createTransport({ host, port })
 
-        this.deliveryMessage(message)
-    }
-
-    // made protected for testing :-(
-    protected async deliveryMessage({host, port, ...msg}: Message) {
-        const transport = nodemailer.createTransport({host, port})
-
-        await transport.sendMail(msg)
-    }
+    await transport.sendMail(msg)
+  }
 }
 
-export interface Message extends SMTPTransport.Options, Mail.Options {
-}
+export interface Message extends SMTPTransport.Options, Mail.Options {}
